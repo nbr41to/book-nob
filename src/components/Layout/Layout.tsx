a9a83d019fbfcee0cbcd642c3214072864137805
Knob <59198478+nbr41to@users.noreply.github.com>
2023-05-22 11:21:58 +0900

    .

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,31 +1,17 @@
 import type { FC, ReactNode } from 'react';
-
-import { Footer } from '@/pages/_layout/Footer';
-import { Header } from '@/pages/_layout/Header';
-import { useLoading } from '@/recoil/hooks/useLoading';
-import { LoadingOverlay } from '@mantine/core';
-import { M_PLUS_Rounded_1c } from '@next/font/google';
-
-const mPlusRounded1c = M_PLUS_Rounded_1c({
-  weight: ['400', '700'],
-  variable: '--font-rounded1c',
-  subsets: ['latin'],
-  display: 'swap',
-});
+import { Footer } from '@/components/Layout/Footer';
+import { Header } from '@/components/Layout/Header';
 
 type Props = {
   children: ReactNode;
 };
 
 export const Layout: FC<Props> = ({ children }) => {
-  const loading = useLoading();
-
   return (
-    <div className={mPlusRounded1c.variable}>
+    <div className='h-screen flex flex-col'>
       <Header />
-      <main>{children}</main>
+      <main className='flex-grow'>{children}</main>
       <Footer />
-      <LoadingOverlay visible={loading.is} />
     </div>
   );
 };
